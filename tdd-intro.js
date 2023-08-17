3728b36e846309c78a34e65eec924e9a7a0639f7
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-17 08:09:57 +0100

    Implement reverseString()

--- tdd-intro.js
+++ tdd-intro.js
@@ -2,4 +2,8 @@ function capitalize(word) {
   return word[0].toUpperCase() + word.substr(1);
 }
 
-export default capitalize;
+function reverseString(word) {
+  return word.split('').reverse().join('');
+}
+
+export { capitalize, reverseString };
